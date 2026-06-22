9b367f91ea5d5a8b5b7314bb7aaf4918eada4bb9
hututuQQQ <a962950733@gmail.com>
2026-06-23 02:15:05 +0800

    refactor(agent): export project policy

--- src/agent/projectModifier.ts
+++ src/agent/projectModifier.ts
@@ -1,5 +1,11 @@
 export * from "./project/types";
 export {
+  DEFAULT_PROJECT_POLICY,
+  NEXTJS_APP_ROUTER_PROJECT_POLICY,
+  type ProjectPackagePolicy,
+  type ProjectPolicy,
+} from "./project/projectPolicy";
+export {
   buildModificationContext,
 } from "./project/context";
 export {
